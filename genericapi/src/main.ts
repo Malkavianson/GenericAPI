a25b9ce9cd21e37bfd8aff6b6f12b603ea3238ca
malkavianson <63012624+Malkavianson@users.noreply.github.com>
2022-11-23 20:01:08 -0300

    Feat: add cors, pipes and swagger settings

--- genericapi/src/main.ts
+++ genericapi/src/main.ts
@@ -1,8 +1,39 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import type { NestExpressApplication } from "@nestjs/platform-express";
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+const PORT = process.env.PORT || 3333;
+
+export async function bootstrap(): Promise<void> {
+	console.clear();
+	console.log("Starting and validating");
+
+	const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+		cors: true,
+	});
+
+	app.set("trust proxy", 1);
+
+	app.useGlobalPipes(new ValidationPipe());
+
+	console.log("Server Started\n\nMapping documentation");
+
+	const config = new DocumentBuilder()
+		.setTitle("Generic API")
+		.setDescription("Controller API for educational purpose")
+		.setVersion("1.0")
+		.addTag("Status")
+		.addServer("http://localhost:3333")
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("docs", app, document);
+
+	console.log("Swagger.setup Builded");
+	console.log("Mapping routes:");
+
+	await app.listen(PORT, () => console.log(`App bootstraped at :${PORT}`));
 }
 bootstrap();
